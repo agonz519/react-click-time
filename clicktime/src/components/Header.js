e5ffad9a4c45683d4eff96e5cb9dc25e5da30f07
Alexander <agonz519@gmail.com>
2018-06-19 09:39:04 -0400

    Initial setup of React site with a few elements & styling

---
+++ clicktime/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => (
+  <header className="header">
+    <h1>CLICK TIME</h1>
+    <h4>...like bullet time, except not at all, well except maybe that you have limited time</h4>
+  </header>
+);
+
+export default Header;
---
+++ clicktime/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const styles = {
+  panel: {
+    margin: 20,
+    background: "#e8eaf6"
+  },
+  heading: {
+    background: "#db5708",
+    minHeight: 50,
+    lineHeight: 3.5,
+    fontSize: "1.2rem",
+    color: "white",
+    padding: "0 20px",
+  }
+};
+
+const Navbar = () => (
+  <div style={styles.panel}>
+    <div style={styles.heading}>Click the below pictures without clicking any repeats. If you do, your score will be reset to zero! zip! nada!</div>
+  </div>
+);
+
+export default Navbar;
